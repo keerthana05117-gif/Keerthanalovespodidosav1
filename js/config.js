005121d09bd8f8a7735c6047f3c7caea1428edf3
nemokaushik <87182095+nemokaushik@users.noreply.github.com>
2026-02-16 16:07:14 +0530

    Update AWS Cognito configuration settings

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_tz92E2Vf9', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'lm6h1d07rbgbdtq5n67q0thkj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_0GJklhKl5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5be990c3l7cdrdq799osvjf5s3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://yv8kimvgcd.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 

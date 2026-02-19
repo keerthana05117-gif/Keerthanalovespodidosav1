2f31199066d489bb432ac686b811d78a319cb917
Keerthana_S <keerthana05117@gmail.com>
2026-02-19 15:46:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_0GJklhKl5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5be990c3l7cdrdq799osvjf5s3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_bM1fwFSis', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2gf7io4hpcdhtte0phfg00v3hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://55nc6ve5r0.execute-api.ap-southeast-1.amazonaws.com/v1-stage' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
